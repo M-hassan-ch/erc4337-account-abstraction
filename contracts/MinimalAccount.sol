3f0819f4d7356b46a76435a029ca32455c159a90
Hassan <muhammad.hassan@antematter.io>
2026-03-26 11:26:05 +0500

    added logic for execute function

--- contracts/MinimalAccount.sol
+++ contracts/MinimalAccount.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.28;
 import {IAccount} from "@account-abstraction/contracts/interfaces/IAccount.sol";
 import {
+    IEntryPoint
+} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {
     PackedUserOperation
 } from "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -15,16 +18,39 @@ import {
 } from "@account-abstraction/contracts/core/Helpers.sol";
 
 contract MinimalAccount is Ownable, IAccount {
-    constructor() Ownable(msg.sender) {}
+    IEntryPoint public i_entrypoint;
+
+    constructor(address entrypoint) Ownable(msg.sender) {
+        i_entrypoint = IEntryPoint(entrypoint);
+    }
+
+    function execute(
+        address dest,
+        uint256 value,
+        bytes calldata functionData
+    ) external {
+        _requireFromEntrypointOrOwner();
+
+        (bool success, ) = dest.call{value: value}(functionData);
+        require(success, "execute call failed");
+    }
 
     function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
     ) external returns (uint256 validationData) {
+        _requireFromEntrypoint();
         validationData = _validateSignature(userOp, userOpHash);
+        _payPreFund(missingAccountFunds);
         return 0;
     }
+
+    function updateEntrypoint(address entrypoint) public onlyOwner() {
+        i_entrypoint = IEntryPoint(entrypoint);
+    }
+
+    receive() external payable{}
 
     function _validateSignature(
         PackedUserOperation calldata userOp,
@@ -38,5 +64,28 @@ contract MinimalAccount is Ownable, IAccount {
             return SIG_VALIDATION_FAILED;
         }
         return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPreFund(uint amount) private {
+        if (amount != 0) {
+            (bool success, ) = payable(msg.sender).call{
+                value: amount,
+                gas: type(uint256).max
+            }("");
+        }
+    }
+
+    function _requireFromEntrypoint() private {
+        require(
+            msg.sender == address(i_entrypoint),
+            "Caller is not a valid entrypint"
+        );
+    }
+
+    function _requireFromEntrypointOrOwner() private {
+        require(
+            msg.sender == address(i_entrypoint) || msg.sender == owner(),
+            "Caller is not a valid entrypint"
+        );
     }
 }
