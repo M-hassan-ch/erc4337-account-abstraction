cbcab0f5c7125c114362b70a5d6cca0918fd2300
Hassan <muhammad.hassan@antematter.io>
2026-03-27 23:52:32 +0500

    Added sample entrypoint contract for local testing

---
+++ contracts/SampleEntryPoint.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {EntryPoint} from "@account-abstraction/contracts/core/EntryPoint.sol";
+
+contract SampleEntryPoint is EntryPoint{
+    // extending all the logic from the parent contract
+    constructor() EntryPoint()  {
+    }
+}
