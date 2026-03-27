a155c89d78a2f2f9afc130f84522f90d74d05f52
Hassan <muhammad.hassan@antematter.io>
2026-03-27 07:29:35 +0500

    updated config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,45 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { loadEnvFile } from "process";
+loadEnvFile();
+
+const RPC_URL = process.env.RPC_URL!;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      evmVersion: "cancun",
+    }
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY as `0x${string}`],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    },
+    customChains: [
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: `https://api.etherscan.io/v2/api?chainid=11155111&apikey=${ETHERSCAN_API_KEY}`,
+          browserURL: "https://sepolia.etherscan.io/"
+        }
+      }
+    ]
+  },
+  sourcify: {
+    enabled: true
+  }
 };
 
 export default config;
