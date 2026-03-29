33c07e9eb9ea523cc85c36956727c1ade597b81a
Hassan <muhammad.hassan@antematter.io>
2026-03-29 12:20:37 +0500

    refactored  user OP broadcasting script

--- scripts/signUserOp.ts
+++ scripts/signUserOp.ts
@@ -1,39 +1,40 @@
 import { assert } from 'node:console';
 import { ethers } from 'hardhat';
-import { SampleEntryPoint } from '../typechain-types';
 
-async function handleOps(signer: any, contract: SampleEntryPoint, userOps: any, beneficiery: any) {
-    try {
-        await contract.connect(signer).handleOps(userOps, beneficiery);
-    } catch (error) {
-        console.log(error);
-    }
+function getEncodedSignature(r: string, s: string, v: number): string {
+    const types = ["bytes32", "bytes32", "uint8"];
+    const values = [r, s, v];
+    const encodedSig = ethers.solidityPacked(types, values);
+    return encodedSig;
 }
 
 async function main() {
-    const smartAccountAddress = "0xc5a5C42992dECbae36851359345FE25997F5C42d";
-    const counterAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+    const smartAccountAddress = process.env.SMART_ACCOUNT_ADDRESS!;
+    const counterAddress = process.env.COUNTER_ADDRESS!;
     const entryPointAddress = process.env.ENTRYPOINT_ADDRESS!;
-    const USER_OP_SIGNER_KEY = process.env.USER_OP_SIGNER_PRIVATE_KEY!;
+    const user_op_signer_key = process.env.USER_OP_SIGNER_PRIVATE_KEY!;
 
-    if (!USER_OP_SIGNER_KEY) {
+    if (!user_op_signer_key) {
         throw new Error("USER_OP_SIGNER_KEY required");
+    }
+    if (!smartAccountAddress || !counterAddress || !entryPointAddress) {
+        throw new Error("SMART_ACCOUNT_ADDRESS, COUNTER_ADDRESS, ENTRYPOINT_ADDRESS required");
     }
 
     const smartAccountContract = await ethers.getContractAt("SmartAccount", smartAccountAddress);
     const counterContract = await ethers.getContractAt("Counter", counterAddress);
     const entryPointContract = await ethers.getContractAt("SampleEntryPoint", entryPointAddress);
 
-    const [signer, bundler, i] = await ethers.getSigners();
-    const userOpSigner = new ethers.Wallet(USER_OP_SIGNER_KEY, ethers.provider);
+    const [bundler] = await ethers.getSigners();
+    const userOpSigner = new ethers.Wallet(user_op_signer_key, ethers.provider);
     const userOpSignerBalance = await ethers.provider.getBalance(userOpSigner);
+    const userOpSignerAddress = await userOpSigner.getAddress();
+    const bundlerAddress = await bundler.getAddress();
+
     console.log("Balance of signer initialing gasless transaction: ", ethers.formatEther(userOpSignerBalance).toString());
 
-    const endUserAddress = await userOpSigner.getAddress();
-    const bundlerAddress = await bundler.getAddress();
-    // const signerAddress = await signer.getAddress();
-
-    const newValue = 100;
+    // Preparing calldata for counter.updateTo(value)
+    const newValue = 386;
     const functionCallData = counterContract.interface.encodeFunctionData("updateTo", [
         newValue,
     ]);
@@ -43,75 +44,75 @@ async function main() {
         functionCallData
     ]);
 
-    // 1. Get digest to sign
-    console.log("1. Getting digest to sign...")
-    const _nonce = await entryPointContract.getNonce(smartAccountAddress, 0);
-    console.log("nonce: ", _nonce);
-
-    const [digest, unSignedUserOp] = await smartAccountContract.getUserOpDigest(
+    // 1. Prepare unsigned UserOp to sign
+    console.log("1. Getting unsigned UserOp to sign...")
+    const smartAccountNonce = await entryPointContract.getNonce(smartAccountAddress, 0);
+    const unSignedUserOp = await smartAccountContract.getUnSignedUserOp(
         executeCalldata,
         smartAccountAddress,
-        _nonce,
+        smartAccountNonce,
     )
-    //
+
     const { sender, nonce, initCode, callData, accountGasLimits, preVerificationGas, gasFees, paymasterAndData, signature } = unSignedUserOp;
-    const _unSignedUserOp = {
+    const _unSignedUserOp = { // Extra step, cannot pass `unSignedUserOp` as it is
         sender, nonce, initCode, callData, accountGasLimits, preVerificationGas, gasFees, paymasterAndData, signature
     }
     const opHash = await entryPointContract.getUserOpHash(_unSignedUserOp);
 
 
     // 2. Sign
-    console.log("2. Signing the digest...")
+    console.log("2. Signing the UserOp...")
     const hexSig = await userOpSigner.signMessage(ethers.getBytes(opHash))
     const sig = ethers.Signature.from(hexSig);
     const r = sig.r;
     const s = sig.s;
     const v = sig.v;
+    const encodedSig = getEncodedSignature(r, s, v);
 
-    // 3. Get Signed UserOp
+    // 3. Verifying signature
+    /*
+        Note: 
+        - getEncodedSignature(), smartAccountContract.getSignedUserOp(), hexSig 
+        - All of the above return the same hash, iam using encodedSig just to be on a safer side
+    */
     console.log("3. Verifying the signature...")
-    const signedUserOp = await smartAccountContract.generateSignedUserOp(_unSignedUserOp, r, s, v);
-    const rawUserOpHash = await entryPointContract.getUserOpHash(_unSignedUserOp);
-    const formattedMesageHash = await smartAccountContract.formatHash(rawUserOpHash);
-
-    const recoveredAddress = ethers.verifyMessage(ethers.getBytes(rawUserOpHash), signedUserOp.signature);
+    const recoveredAddress = ethers.verifyMessage(ethers.getBytes(opHash), encodedSig);
     console.log({
-        signer: endUserAddress,
+        signer: userOpSignerAddress,
         recoveredSignerAddress: recoveredAddress
     });
-    assert(endUserAddress.toLowerCase() == recoveredAddress.toLowerCase(), "Recovered signer did'nt matched with the actual signer");
+    assert(userOpSignerAddress.toLowerCase() == recoveredAddress.toLowerCase(), "Recovered signer did'nt matched with the actual signer");
 
-    // 4. Initiating the transaction through ALT-Mempool/Bundler
-    const balanceBefore = await ethers.provider.getBalance(smartAccountAddress);
+    // 5. Initiating the transaction through ALT-Mempool/Bundler on the behalf of `userOpSigner`
+    const paymasterBalanceBefore = await ethers.provider.getBalance(smartAccountAddress);
+    const bundlerBalanceBefore = await ethers.provider.getBalance(bundlerAddress);
     const valueBefore = await counterContract.value();
 
-    // await smartAccountContract.connect(i).transferOwnership(await userOpSigner.getAddress());
-    // const tx = await signer.sendTransaction({
-    //     to: smartAccountAddress,
-    //     value: ethers.parseUnits("1", "ether")
-    // })
-    // await tx.wait();
-    // console.log(await smartAccountContract.owner());
-
-
-    await handleOps(bundler, entryPointContract, [{
+    const tx = await entryPointContract.connect(bundler).handleOps([{
         ..._unSignedUserOp,
-        signature: signedUserOp.signature
+        signature: encodedSig
     }], bundlerAddress);
-    // await entryPointContract.connect(bundler).handleOps([{
-    //     ..._unSignedUserOp,
-    //     signature: signedUserOp.signature
-    // }], bundlerAddress);
+    await tx.wait();
 
-    const balanceAfter = await ethers.provider.getBalance(smartAccountAddress);
+    const paymasterBalanceAfter = await ethers.provider.getBalance(smartAccountAddress);
+    const bundlerBalanceAfter = await ethers.provider.getBalance(bundlerAddress);
     const valueAfter = await counterContract.value();
-    const balanceDifference = balanceBefore - balanceAfter;
-    console.log("Value before: ", valueBefore.toString());
-    console.log("Value After: ", valueAfter.toString());
-    console.log("Balance before: ", ethers.formatEther(balanceBefore));
-    console.log("Balance after: ", ethers.formatEther(balanceAfter));
-    console.log("Balance changes", ethers.formatEther(balanceDifference))
+    const paymasterBalanceDifference = paymasterBalanceAfter - paymasterBalanceBefore;
+    const bundlerBalanceDifference = bundlerBalanceAfter - bundlerBalanceBefore;
+
+    console.log('----------------------------------------');
+    console.log("Counter Value before: ", valueBefore.toString());
+    console.log("Counter Value After: ", valueAfter.toString());
+    console.log("Counter Value Expected: ", newValue.toString());
+    console.log('----------------------------------------');
+    console.log("Paymaster Balance before: ", ethers.formatEther(paymasterBalanceBefore));
+    console.log("Paymaster Balance after: ", ethers.formatEther(paymasterBalanceAfter));
+    console.log("Paymaster Balance changes", ethers.formatEther(paymasterBalanceDifference));
+    console.log('----------------------------------------');
+    console.log("Bundler Balance before: ", ethers.formatEther(bundlerBalanceBefore));
+    console.log("Bundler Balance after: ", ethers.formatEther(bundlerBalanceAfter));
+    console.log("Bundler Balance changes", ethers.formatEther(bundlerBalanceDifference));
+    console.log('----------------------------------------');
 }
 
 main()
